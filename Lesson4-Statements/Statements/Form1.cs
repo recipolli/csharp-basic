610c4b785e55aa7dbb39cf96b8de2c955c2c0834
unknown <blue15@RM02-16.fasfg.local>
2015-06-22 15:45:44 +0100

    Statement

---
+++ Lesson4-Statements/Statements/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Statements
+{
+    public partial class Form1 : Form
+    {          
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (boxNames.Text != "Renata")
+            {
+                // nested if and else
+                if (boxNames.Text == "Rena" || boxNames.Text == "Re")
+                {
+                    MessageBox.Show("You picked wrong name, try again!");
+                }
+                else
+                {
+                    MessageBox.Show("You have to find the correct name!");
+                }                
+            } // end if
+            else
+            {
+                MessageBox.Show("Congrats! You picked the correct name Renata");
+            } // end else          
+        } 
+    }
+}
